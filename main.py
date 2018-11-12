345e8adf9b0e533c06323e8d0e8cd876f5d9226b
Bruno Codeman <brunobemfica@gmail.com>
2018-11-12 17:57:47 -0200

    - Creating a basic chatbot prototype

---
+++ main.py
@@ -0,0 +1,31 @@
+import asyncio
+import logging
+import uvloop
+from aiohttp import web
+
+intents = { "Bom dia. Tudo bem?": "Bom dia! Eu estou bem e você?", 
+            "Tchau": "Até logo, nos vemos em breve!"}
+
+async def index(request):
+    return web.Response(text="hello")
+
+
+async def chat_request(request):
+    phrase = await request.json()
+    intent = phrase["intent"]
+    return web.json_response({"text": intents[intent]})
+
+async def config_app():
+    asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+    app = web.Application(loop=asyncio.get_event_loop())
+    app.router.add_get("/", index)
+    app.router.add_post("/chat", chat_request)
+    return app
+
+def main():
+    logging.basicConfig(level=logging.DEBUG)
+    app = config_app()
+    web.run_app(app)
+
+if __name__ == '__main__':
+    main()
